70e7dbf1660043ef731813e956c50e2505caaba8
Martín Negro <mnegro91@gmail.com>
2021-08-02 20:35:57 -0300

    Configurado un pre de ruta /article e instalado uuid

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,15 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const article = require('./article')
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.use('/article', article)
 
 
 module.exports = router;
