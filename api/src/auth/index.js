4f8a363951c4f8bc3bfe4da7cda494dab1d46f73
juan gonzalez venzano <juangonzalez465@gmail.com>
2021-08-03 19:27:07 -0300

    agregamos permisos al post de article, y ordenamiento en carpeta auth

---
+++ api/src/auth/index.js
@@ -0,0 +1,23 @@
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const jwtAuthz = require("express-jwt-authz");
+require("dotenv").config();
+const { JWKSURI, AUDIENCE, ISSUER, ALGORITHMS } = process.env;
+const authorizeAccessToken = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${JWKSURI}/.well-known/jwks.json`,
+  }),
+  audience: AUDIENCE,
+  issuer: `https://${JWKSURI}/`,
+  algorithms: [`${ALGORITHMS}`],
+});
+
+const checkAdminPermission = jwtAuthz(["read:messages"], {
+  customScopeKey: "permissions",
+  checkAllScopes: true,
+});
+
+module.exports = { authorizeAccessToken, checkAdminPermission };
--- client/src/views/Home/index.js
+++ client/src/views/Home/index.js
@@ -1,71 +1,68 @@
-import { LoginButton, LogoutButton } from '../../components';
-import { useAuth0 } from '@auth0/auth0-react';
-import axios from 'axios';
-const URL_API = 'http://localhost:3001';
+import { LoginButton, LogoutButton } from "../../components";
+import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+const URL_API = "http://localhost:3001";
 
 const Home = () => {
-	const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
+  const { isAuthenticated, user, getAccessTokenSilently } = useAuth0();
 
-	async function callApiPublicRoute() {
-		try {
-			const response = await axios.get(`${URL_API}/public`);
-			console.log(response.data);
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+  async function callApiPublicRoute() {
+    try {
+      const response = await axios.get(`${URL_API}/public`);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-	async function callApiPrivateRoute() {
-		try {
-			const token = await getAccessTokenSilently();
-			const response = await axios.get(`${URL_API}/private`, {
-				headers: { authorization: `Bearer ${token}` },
-			});
-			console.log(response.data);
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+  async function callApiPrivateRoute() {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await axios.get(`${URL_API}/article`, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-	async function callApiPrivateScopedRoute() {
-		try {
-			const token = await getAccessTokenSilently();
-			const response = await axios.get(`${URL_API}/private-scoped`, {
-				headers: { authorization: `Bearer ${token}` },
-			});
-			console.log(response.data);
-		} catch (error) {
-			console.log(error.message);
-		}
-	}
+  async function callApiPrivateScopedRoute() {
+    try {
+      const token = await getAccessTokenSilently();
+      const response = await axios.get(`${URL_API}/private-scoped`, {
+        headers: { authorization: `Bearer ${token}` },
+      });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
-	return (
-		<div>
-			{isAuthenticated ? (
-				<div>
-					<h1>pagina de Home estando logeado</h1>
-					<pre style={{ textAlign: 'center' }}>
-						{JSON.stringify(user, null, 2)}
-					</pre>
-					<LogoutButton btnText="Cerrar session" />
-				</div>
-			) : (
-				<div>
-					<h1>pagina de Home sin logear</h1>
-					<LoginButton btnText="Comencemos" loginWith="Popup" />
-				</div>
-			)}
-			<div>
-				<button onClick={callApiPublicRoute}>Call API public route</button>
-
-				<button onClick={callApiPrivateRoute}>Call API private route</button>
-
-				<button onClick={callApiPrivateScopedRoute}>
-					Call API private-scoped route
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      {isAuthenticated ? (
+        <div>
+          <h1>pagina de Home estando logeado</h1>
+          <pre style={{ textAlign: "center" }}>
+            {JSON.stringify(user, null, 2)}
+          </pre>
+          <LogoutButton btnText="Cerrar session" />
+        </div>
+      ) : (
+        <div>
+          <h1>pagina de Home sin logear</h1>
+          <LoginButton btnText="Comencemos" loginWith="Popup" />
+        </div>
+      )}
+      <div>
+        {/* BOTON SOLO DE PRUEBA,SIN PERMISO NI DE AUTENTICACION */}
+        <button onClick={callApiPublicRoute}>Call API public route</button>
+        {/* BOTON QUE SERIA PARA CREAR UN POST, PERMISO DE ADMIN */}
+        <button onClick={callApiPrivateRoute}>Call API private route</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
