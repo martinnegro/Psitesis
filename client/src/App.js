e2a7cfd78a8f51c4814471fb3c535fe1d0ccef06
Martín Negro <mnegro91@gmail.com>
2021-08-04 18:47:51 -0300

    merging

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import "./App.css";
-import { useAuth0 } from "@auth0/auth0-react";
-import { Route, Switch } from "react-router-dom";
-import { Loading, ProtectedRoute } from "./components";
-import Login from "./views/Login";
-import ExamplePage from "./views/ExamplePage";
-=======
 import { useAuth0 } from '@auth0/auth0-react';
 import { Route, Switch } from 'react-router-dom';
 import { Loading } from './components';
 import  Login  from './components/Login/index'
 import  Home  from './views/Home/Home.jsx'
 import  Post from './views/Post/Post.jsx'
->>>>>>> main
 
 const App = () => {
   const { isLoading } = useAuth0();
@@ -25,16 +16,6 @@ const App = () => {
     );
   }
 
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <ProtectedRoute path="/example" exact component={ExamplePage} />
-      </Switch>
-    </div>
-  );
-=======
 	return (
 		<div className="App">
 			<Switch>
@@ -44,7 +25,6 @@ const App = () => {
 			</Switch>
 		</div>
 	);
->>>>>>> main
 };
 
 export default App;
