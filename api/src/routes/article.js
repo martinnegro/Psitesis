a734b6525aa3cb989ed78f977685f98bd9aa0ac5
juan gonzalez venzano <juangonzalez465@gmail.com>
2021-08-04 18:19:00 -0300

    config auth0 folder,clean log in component,moving callApiPost function to actions

--- api/src/routes/article.js
+++ api/src/routes/article.js
@@ -1,40 +1,44 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require("uuid");
+const { authorizeAccessToken, checkAdminPermission } = require("../auth/index");
 
-router.post('/', (req, res, next) => {
+router.post(
+  "/",
+  authorizeAccessToken,
+  checkAdminPermission,
+  (req, res, next) => {
     const id = uuidv4();
     console.log(req.body);
-    res.json({message: 'Artículo recibido', id })
+    res.json({ message: "Artícle recieved", id });
+  }
+);
+
+router.get("/", (req, res, next) => {
+  console.log(req.query);
+  res.json(req.query);
 });
 
-router.get('/', (req, res, next) => {
-    console.log(req.query);
-    res.json(req.query);
+router.get("/:id", (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  if (!id) {
+    const err = new Error("No ID");
+    err.status = 400;
+    next(err);
+  } else res.json({ id });
 });
 
-router.get('/:id', (req, res, next) => {
-    const { id } = req.params;
-    console.log(id);
-    if (!id) {
-        const err = new Error ('No hay id');
-        err.status = 400;
-        next(err)
-    } else res.json({ id });
+router.put("/:id", (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  res.json({ message: `ID recieved: ${id}` });
 });
 
-router.put('/:id', (req, res, next) => {
-    const { id } = req.params;
-    console.log(id);
-    res.json({ message: `ID recibido: ${id}`})
+router.delete("/:id", (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  res.json({ message: `ID recived: ${id}` });
 });
-
-router.delete('/:id', (req, res, next) => {
-    const { id } = req.params;
-    console.log(id);
-    res.json({ message: `ID recibido: ${id}`})
-});
-
-
 
 module.exports = router;
