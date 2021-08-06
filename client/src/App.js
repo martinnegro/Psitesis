955bc07eb5dd1a7e9b45182e2f41a1230882356f
facundomaksud@gmail.com <facundomaksud@gmail.com>
2021-08-05 21:19:48 -0300

    Landingpage y componentes modificados

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Route, Switch } from "react-router-dom";
 import { Loading } from "./components";
-import Login from "./components/Login/index";
+import Landing from "./components/Landing/Landing";
 import Art_Detail from "./views/Art_Detail/Art_Detail";
 import Home from "./views/Home/Home.jsx";
 import Post from "./views/Post/Post.jsx";
+
 
 const App = () => {
   const { isLoading } = useAuth0();
@@ -20,7 +21,7 @@ const App = () => {
   return (
     <div className="App">
       <Switch>
-        <Route path="/" exact component={Login} />
+        <Route path="/" exact component={Landing} />
         <Route path="/home" exact component={Home} />
         <Route path="/post" exact component={Post} />
         <Route path="/post/:id" exact component={Art_Detail} />
--- client/src/components/LoginButton/index.js
+++ client/src/components/LoginButton/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import {purple } from '@material-ui/core/colors';
+
+const ColorButton = withStyles((theme) => ({
+	root: {
+	  color: theme.palette.getContrastText(purple[500]),
+	  backgroundColor: purple[500],
+	  '&:hover': {
+		backgroundColor: purple[700],
+	  },
+	},
+  }))(Button);
+  
+  const useStyles = makeStyles((theme) => ({
+	margin: {
+	  margin: theme.spacing(1),
+	},
+  }));
 
 const LoginButton = ({ btnText, loginWith }) => {
+	console.log(btnText, "aaaa")
 	const { loginWithPopup, loginWithRedirect } = useAuth0();
-
+	const classes = useStyles();
 	function login() {
 		switch (loginWith) {
 			case 'Popup':
@@ -18,7 +38,7 @@ const LoginButton = ({ btnText, loginWith }) => {
 		}
 	}
 
-	return <button onClick={() => login()}>{btnText}</button>;
+	return <ColorButton onClick={() => login()}>{btnText}</ColorButton>;
 };
 
 export default LoginButton;
