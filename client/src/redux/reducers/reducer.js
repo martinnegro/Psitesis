553f56fad711023c64349c9d56b677a949ecbec5
Carlos Alberto <karlosagreda@hotmail.com>
2021-08-06 20:29:05 -0300

    refactory

--- client/src/redux/reducers/reducer.js
+++ client/src/redux/reducers/reducer.js
@@ -1,33 +1,49 @@
-// import { GET_ALL_ARTICLE, GET_ARTICLE_DETAIL } from '../actions/actions'
-
-import { GET_ALL_ARTICLE, GET_ARTICLE_DETAIL, GET_USERS } from "../actions/actions";
+import {
+	GET_ALL_ARTICLE,
+	GET_ARTICLE_DETAIL,
+	GET_USERS,
+	SET_USER_ID,
+	SET_USER_ROLES,
+} from '../actions/actions';
 
 const initialState = {
-  articles: [],
-  articlesDetail: [],
-  users: []
+	articles: [],
+	articlesDetail: undefined,
+	users: [],
+	user_id: undefined,
+	user_roles: [],
 };
 
 function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    case GET_ALL_ARTICLE:
-      return {
-        ...state,
-        articles: action.payload
-      }
-    case GET_ARTICLE_DETAIL:
-      return {
-        ...state,
-        articlesDetail: action.payload
-      }
-    case GET_USERS:
-      return {
-        ...state,
-        users: action.payload
-      }
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case GET_ALL_ARTICLE:
+			return {
+				...state,
+				articles: action.payload,
+			};
+		case GET_ARTICLE_DETAIL:
+			return {
+				...state,
+				articlesDetail: action.payload,
+			};
+		case GET_USERS:
+			return {
+				...state,
+				users: action.payload,
+			};
+		case SET_USER_ID:
+			return {
+				...state,
+				user_id: action.payload,
+			};
+		case SET_USER_ROLES:
+			return {
+				...state,
+				user_roles: action.payload,
+			};
+		default:
+			return state;
+	}
 }
 
 export default rootReducer;
