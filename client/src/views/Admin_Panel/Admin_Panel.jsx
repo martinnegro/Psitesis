e931cfb99dd1412680064e660a3fc150bd6b2360
Martín Negro <mnegro91@gmail.com>
2021-08-07 23:31:54 -0300

    commit para merge

--- client/src/views/Admin_Panel/Admin_Panel.jsx
+++ client/src/views/Admin_Panel/Admin_Panel.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Container, Typography, makeStyles } from '@material-ui/core';
+import Accordion from '@material-ui/core/Accordion';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 
 import Nav from "../../components/Nav/Nav";
 
 const useStyles = makeStyles((theme) => ({
     offset: theme.mixins.toolbar,
-    Home: {
+    Admin_Panel: {
       // marginLeft: theme.spacing(15),
       marginTop: theme.spacing(12),
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
       justifyContent: "center",
+    },
+    root: {
+        width: "100%",
     }
 }));
 
@@ -21,12 +28,38 @@ const Admin_Panel = () => {
     return (
         <Container >
             <Nav />
-                <Container className={classes.Home}>
+                <Container className={classes.Admin_Panel}>
                     <Typography
                     variant="h2" color="initial"
                     >
                     Panel de Administrador
                     </Typography>
+                    <div className={classes.root}>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1bh-content"
+                            id="panel1bh-header"
+                        >
+                            Instituciones
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            Mundo
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1bh-content"
+                            id="panel1bh-header"
+                        >
+                            Categorías
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            Mundo
+                        </AccordionDetails>
+                    </Accordion>
+                    </div>
                 </Container>
           
         </Container>
