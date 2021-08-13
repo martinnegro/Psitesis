a0fed39a775f9a135ecb929306bfa3fad483bc33
Carlos Alberto <karlosagreda@hotmail.com>
2021-08-13 17:32:04 -0300

    fix

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,23 +1,23 @@
-import { LoginButton } from '..';
+//import { LoginButton } from '..';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Redirect } from 'react-router-dom';
-import styles from "./Landing.module.css"
-import IMG from "../../assets/imgLanding.jpg"
+//import styles from './Landing.module.css';
+//import IMG from '../../assets/imgLanding.jpg';
 
 const Landing = () => {
-	const { isAuthenticated } = useAuth0();
+	const { isAuthenticated, loginWithRedirect } = useAuth0();
 
 	return (
 		<div>
 			{isAuthenticated ? (
 				<Redirect to="/home" />
 			) : (
-				
-				<div className={styles.container}>
+				loginWithRedirect()
+				/*<div className={styles.container}>
 					<img src={IMG} className={styles.image} alt={"aaaa"}/>
 					<h4 className={styles.information}>Encontra ARTÍCULOS escritos por expertos para ayudarte a hacer tu tesis</h4>
 					<LoginButton btnText="Comencemos" loginWith="Redirect" />
-				</div>
+				</div>*/
 			)}
 			<div></div>
 		</div>
