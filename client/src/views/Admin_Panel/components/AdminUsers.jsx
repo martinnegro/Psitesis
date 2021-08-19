b05be0beb4ed2658b6a87dd37bbed37198a3258f
Martín Negro <mnegro91@gmail.com>
2021-08-18 23:23:36 -0300

    checkout

--- client/src/views/Admin_Panel/components/AdminUsers.jsx
+++ client/src/views/Admin_Panel/components/AdminUsers.jsx
@@ -10,11 +10,13 @@ import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import IconButton from '@material-ui/core/IconButton';
+import Avatar from '@material-ui/core/Avatar';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import LastPageIcon from '@material-ui/icons/LastPage';
 import { TableHead } from '@material-ui/core';
+import { useSelector } from 'react-redux';
 
 const useStyles1 = makeStyles((theme) => ({
   root: {
@@ -81,26 +83,6 @@ TablePaginationActions.propTypes = {
   rowsPerPage: PropTypes.number.isRequired,
 };
 
-function createData(name, calories, fat) {
-  return { name, calories, fat };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7),
-  createData('Donut', 452, 25.0),
-  createData('Eclair', 262, 16.0),
-  createData('Frozen yoghurt', 159, 6.0),
-  createData('Gingerbread', 356, 16.0),
-  createData('Honeycomb', 408, 3.2),
-  createData('Ice cream sandwich', 237, 9.0),
-  createData('Jelly Bean', 375, 0.0),
-  createData('KitKat', 518, 26.0),
-  createData('Lollipop', 392, 0.2),
-  createData('Marshmallow', 318, 0),
-  createData('Nougat', 360, 19.0),
-  createData('Oreo', 437, 18.0),
-].sort((a, b) => (a.calories < b.calories ? -1 : 1));
-
 const useStyles2 = makeStyles({
   table: {
     minWidth: 500,
@@ -109,17 +91,18 @@ const useStyles2 = makeStyles({
 
 export default function AdminUsers() {
   const classes = useStyles2();
+  const users = useSelector(state => state.usersReducer.users)
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [usersPerPage, setUsersPerPage] = React.useState(5);
 
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+  const emptyusers = usersPerPage - Math.min(usersPerPage, users.length - page * usersPerPage);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
+  const handleChangeUsersPerPage = (event) => {
+    setUsersPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
 
@@ -133,25 +116,25 @@ export default function AdminUsers() {
             <TableCell></TableCell>
         </TableHead>
         <TableBody>
-          {(rowsPerPage > 0
-            ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            : rows
+          {(usersPerPage > 0
+            ? users.slice(page * usersPerPage, page * usersPerPage + usersPerPage)
+            : users
           ).map((row) => (
             <TableRow key={row.name}>
               <TableCell component="th" scope="row">
-                {row.name}
+                <Avatar alt={row.user_name} src={row.user_img_profile}/>
               </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.calories}
+              <TableCell /*style={{ width: 160 }}*/ align="left">
+                {row.user_name}
               </TableCell>
-              <TableCell style={{ width: 160 }} align="right">
-                {row.fat}
+              <TableCell /*style={{ width: 160 }}*/ align="right">
+                
               </TableCell>
             </TableRow>
           ))}
 
-          {emptyRows > 0 && (
-            <TableRow style={{ height: 53 * emptyRows }}>
+          {emptyusers > 0 && (
+            <TableRow style={{ height: 53 * emptyusers }}>
               <TableCell colSpan={6} />
             </TableRow>
           )}
@@ -159,17 +142,17 @@ export default function AdminUsers() {
         <TableFooter>
           <TableRow>
             <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              usersPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
               colSpan={3}
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
+              count={users.length}
+              usersPerPage={usersPerPage}
               page={page}
               SelectProps={{
                 inputProps: { 'aria-label': 'rows per page' },
                 native: true,
               }}
               onPageChange={handleChangePage}
-              onRowsPerPageChange={handleChangeRowsPerPage}
+              onRowsPerPageChange={handleChangeUsersPerPage}
               ActionsComponent={TablePaginationActions}
             />
           </TableRow>
