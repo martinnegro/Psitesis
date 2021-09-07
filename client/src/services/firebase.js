b76c7dc3514ead1936221968806cc0b9bf5155e1
ciroribba <ciro.ribba@gmail.com>
2021-09-07 14:27:14 -0300

    actualizo credenciales firebase

--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/storage';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBv_ElZ4EyrvpvO8vxaZU0Bpax9Ge3L6wE",
-    authDomain: "prueba-psitesis.firebaseapp.com",
-    projectId: "prueba-psitesis",
-    storageBucket: "prueba-psitesis.appspot.com",
-    messagingSenderId: "172975466734",
-    appId: "1:172975466734:web:1da21e720b909025aeebff"
+  apiKey: "AIzaSyBtdO29jlJ1MhGI730V12oUD0n-8WqbVCU",
+  authDomain: "psitesis-4ac10.firebaseapp.com",
+  projectId: "psitesis-4ac10",
+  storageBucket: "psitesis-4ac10.appspot.com",
+  messagingSenderId: "643065385730",
+  appId: "1:643065385730:web:881a3dc2aed88047b8a81b"
   }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
